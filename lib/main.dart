04fc4e06be42b9722bba449220746a6de235956e
ivor.norval@gmail.com <ivor.norval@gmail.com>
2021-10-06 05:40:49 +0200

    block landscape

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'screens/home_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
   await Firebase.initializeApp().whenComplete(() => runApp(const MyApp()));
 }
 
